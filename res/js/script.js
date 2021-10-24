1d7ab863075b18d292211c0e9c818bbca4d087c2
Hanna Brit Jõgis <hannabritjg@gmail.com>
2021-10-24 15:05:55 +0300

    updated webpage to fetch information from json (online and from local json file)

--- res/js/script.js
+++ res/js/script.js
@@ -17,3 +17,82 @@ window.onclick = function (event) {
     }
   }
 };
+
+$(function() {
+  /* $.get("https://private-5a8573-postscontainer.apiary-mock.com/posts", function(posts) {
+    for (post of posts) {
+      let avatar = post.author.avatar;
+      let name = post.author.name;
+      let time = post["create-time"];
+      //let type = post.media.type;
+      let url = post.media.url;
+      let text = post["post-text"];
+      let likeURL = post["like-icon"]["url"];
+      let iconText = post["like-icon"]["icon-text"];
+
+      let postContainer = $('<div id= "post" class= "post">');
+      let authorContainer = $('<div id= "author" class = "post-header">');
+      let textContainer = $('<p id="post-text">');
+      let likeButton = $('<button id="like-icon">');
+      let iconTextContainer = $('<div id="icon-text">');
+      
+
+
+      authorContainer.append($('<img id ="avatar" src= "' + avatar + '"/>'));
+      authorContainer.append($('<h5 id= "name">').text(name));
+      postContainer.append(authorContainer);
+      postContainer.append($('<h5 id= "create-time">').text(time));
+      postContainer.append($('<img id = "media" class= "post-image" src= "' + url + '"/>'))
+      textContainer.append(text);
+      postContainer.append(textContainer);
+      likeButton.append($('<img id ="url" src= "' + likeURL + '"/>'));
+      iconTextContainer.append(iconText);
+      likeButton.append(iconTextContainer);
+      postContainer.append(likeButton);
+
+
+
+
+
+      $('ul').append(postContainer); */
+
+      $.get("res/json/posts.json", function(posts) {
+    for (post of posts) {
+      let avatar = post["author"]["avatar"];
+      let name = post["author"]["name"];
+      let time = post["create-time"];
+      //let type = post.media.type;
+      let url = post.media.url;
+      let text = post["post-text"];
+      let likeURL = post["like-icon"]["url"];
+      let iconText = post["like-icon"]["icon-text"];
+
+      let postContainer = $('<div id= "post" class= "post">');
+      let authorContainer = $('<div id= "author" class = "post-header">');
+      let textContainer = $('<p id="post-text">');
+      let likeButton = $('<button id="like-icon">');
+      let iconTextContainer = $('<div id="icon-text">');
+      
+
+
+      authorContainer.append($('<img id ="avatar" src= "' + avatar + '"/>'));
+      authorContainer.append($('<h5 id= "name">').text(name));
+      postContainer.append(authorContainer);
+      postContainer.append($('<h5 id= "create-time">').text(time));
+      postContainer.append($('<img id = "media" class= "post-image" src= "' + url + '"/>'))
+      textContainer.append(text);
+      postContainer.append(textContainer);
+      likeButton.append($('<img id ="url" src= "' + likeURL + '"/>'));
+      iconTextContainer.append(iconText);
+      likeButton.append(iconTextContainer);
+      postContainer.append(likeButton);
+
+
+
+
+
+      $('ul').append(postContainer);
+        
+    }
+  })
+})
